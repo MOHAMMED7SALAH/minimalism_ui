4db6a37b712751ce96a696cfc6befd6df64c3512
MOHAMMED7SALAH <bentitimohammedsalahstars7@gmail.com>
2022-03-18 15:38:38 +0100

    minimalism_ui

---
+++ lib/colors.dart
@@ -0,0 +1,18 @@
+﻿import 'package:flutter/widgets.dart';
+
+const Color backgroundColor = Color.fromARGB(255, 253, 251, 199);
+
+const Color primarybuttonColor = Color.fromARGB(255, 253, 251, 199);
+const Color secondaryButtonColor = Color.fromARGB(255, 253, 251, 199);
+
+const Color iconColor = Color(0xFF8E98AE);
+
+const Color primaryActiveButtonColor = Color(0xFF6D8CFF);
+
+const Color secondaryActiveButtonColor = Color(0xFF6F8AFF);
+
+const Color trackColor = Color(0xFF94ADFB);
+const Color trackAccentColor = Color(0xFFB5C6F5);
+const Color trackVarientColor = Color(0xFF94ADFB);
+
+const Color playerButtonColor = Color(0xFF6887FE);
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'audio_player_screen.dart';
+
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+      DevicePreview(
+        // enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Outfit",
+      ),
+      home: const AudioPlayerScreen(),
+    );
+  }
+}
